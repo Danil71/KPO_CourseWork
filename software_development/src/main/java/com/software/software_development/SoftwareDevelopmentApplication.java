0d058f8a7cd8a820e68062f09f00645dbd7de14a
Даниил Путинцев <daniilputincev91@gmail.com>
2025-11-09 03:30:41 +0400

    Инициализация бекэнда

---
+++ software_development/src/main/java/com/software/software_development/SoftwareDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.software.software_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDevelopmentApplication.class, args);
+	}
+
+}
---
+++ software_development/src/test/java/com/software/software_development/SoftwareDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.software_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
