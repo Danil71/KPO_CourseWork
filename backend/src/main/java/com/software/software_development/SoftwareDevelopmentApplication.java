4f5a37133d4de79c505c28eef77126dea1d75f27
Даниил Путинцев <daniilputincev91@gmail.com>
2025-12-09 22:50:21 +0400

    Заполнение сущностей

--- backend/src/main/java/com/software/software_development/SoftwareDevelopmentApplication.java
+++ backend/src/main/java/com/software/software_development/SoftwareDevelopmentApplication.java
@@ -1,13 +1,31 @@
 package com.software.software_development;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.software.software_development.core.setup.EntityInitializer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class SoftwareDevelopmentApplication {
+public class SoftwareDevelopmentApplication implements CommandLineRunner{
+
+	private final EntityInitializer initializer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoftwareDevelopmentApplication.class, args);
 	}
 
+	@Override
+	@Transactional
+	public void run(String... args) {
+		if (args.length > 0 && Arrays.asList(args).contains("--populate")) {
+			initializer.initializeAll();
+		}
+	}
 }
